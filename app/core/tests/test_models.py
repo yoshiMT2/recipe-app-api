4ddc5edfae675baac301443a7341237115e6777b
Yoshiyuki Matsumoto <81567061+yoshiMT2@users.noreply.github.com>
2022-08-30 10:10:51 +0900

    Added Tag Serializer

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -56,3 +61,10 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
